3d3fce01c4d50594f047927995b55a7899554caf
moniquechimanski <monique.chimanski@escola.pr.gov.br>
2025-05-13 14:28:17 +0000

    ok

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'programação',
+    'O que é HTML',
+    'Linguagem de marcação'
+)
+
+criaCartao(
+    'inglês',
+    'como é dia do pagamento',
+    'Pay Day'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
